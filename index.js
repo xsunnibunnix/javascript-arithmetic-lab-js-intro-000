0660d787514e6cdc3f694aa740712260865aca4b
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-10 16:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a,b) {
 function divide(a,b) {
   return a / b
 }
-increment(n) {
+function increment(n) {
   return n += 1
 }
-decrement(n) {
+function decrement(n) {
   return n -= 1
 }
