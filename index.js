754090cd72c24335cdaf3ed21d7da2d77e143108
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-10 16:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-add()
-subtract()
-multiply()
-divide()
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
